cac9214556290d02f067e7180fd9a8ca281cdacd
ntorrecillas10 <nalascobic7@gmail.com>
2025-02-06 17:34:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/magicjitcompose/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.magicjitcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val RojoCoral = Color(0xFFE3414E)
+val RosaFucsia = Color(0xFFF55281)
+val AzulTurquesa = Color(0xFF1E8A96)
+val VerdeEsmeralda = Color(0xFF228F70)
+val NaranjaCalido = Color(0xFFFF8A40)
+val AmarilloDorado = Color(0xFFFFC72C)
+val VioletaElectrico = Color(0xFF8A4FFF)
+val CianBrillante = Color(0xFF5CC8FF)
+val VerdeLima = Color(0xFF7ED957)
+val GrisNeutro = Color(0xFFD9D9D9)
+val GrisClaro = Color(0xFFD1D1D1)
